d6ca25c295334652b01e71955c4270d17904971e
Julian Muratori <julianmuratori@gmail.com>
2018-03-22 11:14:51 -0400

    finished basic page rendering of search results components

--- src/Components/SingleSearchResult.jsx
+++ src/Components/SingleSearchResult.jsx
@@ -5,11 +5,17 @@ import { Container } from 'bloomer'
 class SingleSearchResult extends Component {
     render() {
         const { title, episodeNumber, details, season } = this.props.episode;
-        return (
-            <Container>
-                <p>{title}</p>
-            </Container>
-        )
+        return <Container>
+            <p>{title}</p>
+            <p>
+              Season {season}, Episode {episodeNumber}
+            </p>
+            <ul>
+                {details.map(detail => {
+                    return <li>{detail}</li>
+                })}
+            </ul>
+          </Container>;
     }
 }
 
